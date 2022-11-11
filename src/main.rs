feb889ab875e88295eb82449d22e7fa09b3a3109
Lawrence Horton <lawrence@hortonium.com>
2022-11-10 21:10:23 -0700

    test3

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,5 @@ use crate::lexer::{execute, parse, Lexer};
 mod lexer;
 
 fn main() {
-    println!("{:?}", Lexer::new().tokenize("5 + 1 + 'hello';"));
+    println!("{:?}", Lexer::new().tokenize("2+1+15 + 1 + 'hello';"));
 }
